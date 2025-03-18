62c1395d33b0245cb200394f7732868dc89cd3a1
MiquelMontero02 <104500695+MiquelMontero02@users.noreply.github.com>
2025-03-18 17:17:25 +0100

    UPDATED: Description shown in console

--- public/JS/videoChapters.js
+++ public/JS/videoChapters.js
@@ -8,7 +8,7 @@ var INIT=0;
 
 // Esperar a que el video cargue y los tracks estén disponibles
 video.addEventListener("loadedmetadata", async ()=>{
-    if(INIT=0){
+    if(INIT==0){
         await updateMediaTracks("tokio",1)
         INIT=1;
     }
