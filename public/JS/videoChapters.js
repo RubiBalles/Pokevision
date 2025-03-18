62352ac936efc975ec4939f7a3a9064962cefad8
RubiBalles <104781672+RubiBalles@users.noreply.github.com>
2025-03-18 17:46:34 +0100

    changes selector

--- public/JS/videoChapters.js
+++ public/JS/videoChapters.js
@@ -9,7 +9,7 @@ var INIT=0;
 // Esperar a que el video cargue y los tracks estén disponibles
 video.addEventListener("loadedmetadata", async ()=>{
     if(INIT==0){
-        await updateMediaTracks("tokio",1)
+        await updateMediaTracks("suiza",1)
         INIT=1;
     }
         });
