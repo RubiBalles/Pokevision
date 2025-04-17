8cef75db283b7bc95e895268dd19cb29c64df2d1
RubiBalles <104781672+RubiBalles@users.noreply.github.com>
2025-04-17 19:02:36 +0200

    changes in controlremoto.html and pokeball effects

--- public/JS/continueVideo.js
+++ public/JS/continueVideo.js
@@ -4,12 +4,12 @@ BUTTON.addEventListener("click",resetPlayer)
 function resetPlayer(){
     if(!BUTTON.hidden){
         gifOverlay.style.display = "none"; // Ocultar GIF
-        gifOverlay.classList.remove("intoPokeball")
-        pokeball.classList.remove("shake")
+        gifOverlay.classList.remove("intoPokeball");
+        pokeball.classList.remove("shake");
         video.style.filter = "none"; // Quitar desenfoque
-        pokeTrainer.style.display="none"
-        pokeball.style.display="none"
-        radioGroup.style.display="none"
+        pokeball.style.display="none";
+        pokeTrainer.style.display="none";
+        radioGroup.style.display="none";
         video.play()
         document.getElementById("pokeAudio").pause();
         BUTTON.hidden=true
